1945b7565f86b70a8c243e6587569b9c3b2b840e
Nick Brown <nicholas.g.brown@gmail.com>
2016-09-12 16:46:14 -0400

    Complete exercise 19.

---
+++ exercise19/ReferencesInMemory.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Example {
+
+}
+
+public class ReferencesInMemory {
+  public static void main(String[] args) {
+
+    int age = 44;
+    String name = "Gosling";
+
+    int[] digits = { 3, 1, 4, 1, 5, 9, 2 };
+    Scanner keyboard = new Scanner(System.in);
+    ArrayList arr = new ArrayList();
+    Integer year = new Integer(1995);
+    Object obj = new Object();
+    Example ex = new Example();
+
+    System.out.println(digits);
+    System.out.println(obj);
+    System.out.println(ex);
+    System.out.println();
+
+    String[] names = { "John", "Paul", "George", "Ringo", "Brian" };
+    System.out.println(names);
+    System.out.println(keyboard);
+    System.out.println(year);
+    System.out.println(name);
+    System.out.println(arr);
+  }
+}
